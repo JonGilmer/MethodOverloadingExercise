1e9e62e1d5b983b414e0194d9984a3931b2105e4
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-18 12:49:21 -0500

    Pushed the completed version of Exercise to GitHub.

--- MethodOverloadingExercise/Program.cs
+++ MethodOverloadingExercise/Program.cs
@@ -6,7 +6,35 @@ namespace MethodOverloadingExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1.12345m, 2.564m));
+            Add(0, 1, true);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            if ((isTrue == true && x + y > 1) || (isTrue == true && x + y == 0))
+            {
+                var dollarsReturn = $"{ x + y } dollars.";
+                Console.WriteLine(dollarsReturn);
+                return dollarsReturn;
+            }
+            else
+            {
+                var singleDollarReturn = $"{ x + y } dollar.";
+                Console.WriteLine(singleDollarReturn);
+                return singleDollarReturn;
+            }
         }
     }
 }
